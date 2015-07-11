c916dfaaba84b88a25a3616edfb59e4dab42f72f
heeroLL <114046323@qq.com>
2015-07-11 09:19:48 +0800

    bis

--- isell/bis/src/test/java/com/sell/ei/sms/controller/SMSControllerTest.java
+++ isell/bis/src/test/java/com/sell/ei/sms/controller/SMSControllerTest.java
@@ -19,4 +19,11 @@ public class SMSControllerTest {
         System.out.println("result=" + result);
     }
     
+    @Test
+    public void testSendTestMessage() {
+        String json = "{\"to\":\"15951227110\",\"param\":\"【这只是一个测试！】\"}";
+        String result = HttpUtils.httpPost("http://localhost:8080/bis/sms/sendTestMessage", json);
+        System.out.println("result=" + result);
+    }
+    
 }
