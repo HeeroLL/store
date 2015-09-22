f0f47a6fc70606f6f38fbb36d2321f95b34ec425
HeeroLL <114046323@qq.com>
2015-09-22 17:13:16 +0800

    增加支付宝批量付款接口

--- isell/bis/src/main/java/com/isell/ei/pay/alipay/service/impl/AlipayServiceImpl.java
+++ isell/bis/src/main/java/com/isell/ei/pay/alipay/service/impl/AlipayServiceImpl.java
@@ -24,8 +24,16 @@ public class AlipayServiceImpl implements AlipayService {
     private Logger log = Logger.getLogger(AlipayServiceImpl.class);
     
     @Override
-    public String getParamInputs(Map<String, String> paramMap) {
-        paramMap = generateMap(paramMap);
+    public String getPayParams(Map<String, String> paramMap) {
+        // 删除空参数
+        paramMap = AlipayUtil.paraFilter(paramMap);
+        paramMap.put("partner", PARTNER);
+        paramMap.put("seller_id", PARTNER);
+        paramMap.put("_input_charset", INPUT_CHARSET);
+        paramMap.put("sign_type", SIGN_TYPE);
+        paramMap.put("payment_type", PAYMENT_TYPE);
+        
+        paramMap.put("sign", AlipayUtil.encryptString(AlipayUtil.getParameter(paramMap), KEY));
         
         StringBuilder sbHtml = new StringBuilder();
         sbHtml.append("<form id=\"alipaysubmit\" name=\"alipaysubmit\" action=\"" + ALIPAY_GATEWAY_NEW + "\" method=\""
@@ -43,23 +51,31 @@ public class AlipayServiceImpl implements AlipayService {
         return sbHtml.toString();
     }
     
-    /**
-     * 设置一些必填参数
-     * 
-     * @param paramMap 参数map
-     * @return 格式化后的参数map
-     */
-    private Map<String, String> generateMap(Map<String, String> paramMap) {
+    @Override
+    public String getBatchTransParams(Map<String, String> paramMap) {
         // 删除空参数
         paramMap = AlipayUtil.paraFilter(paramMap);
         paramMap.put("partner", PARTNER);
-        paramMap.put("seller_id", PARTNER);
         paramMap.put("_input_charset", INPUT_CHARSET);
         paramMap.put("sign_type", SIGN_TYPE);
-        paramMap.put("payment_type", PAYMENT_TYPE);
+        paramMap.put("account_name", ACCOUNT_NAME);
+        paramMap.put("Email", EMAIL);
         
         paramMap.put("sign", AlipayUtil.encryptString(AlipayUtil.getParameter(paramMap), KEY));
         
-        return paramMap;
+        StringBuilder sbHtml = new StringBuilder();
+        sbHtml.append("<form id=\"alipaysubmit\" name=\"alipaysubmit\" action=\"" + ALIPAY_GATEWAY_NEW + "\" method=\""
+            + METHOD + "\">");
+        
+        for (Entry<String, String> entry : paramMap.entrySet()) {
+            sbHtml.append("<input type=\"hidden\" name=\"" + entry.getKey() + "\" value=\"" + entry.getValue() + "\"/>");
+        }
+        
+        // submit按钮控件请不要含有name属性
+        sbHtml.append("<input type=\"submit\" value=\"确认\" style=\"display:none;\"></form>");
+        sbHtml.append("<script>document.forms['alipaysubmit'].submit();</script>");
+        
+        log.info(sbHtml.toString());
+        return sbHtml.toString();
     }
 }
