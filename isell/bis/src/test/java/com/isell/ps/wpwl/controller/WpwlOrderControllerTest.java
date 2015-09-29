95d5b74a7fa0d4f4fd0642da07a36b4726fb6b94
HeeroLL <114046323@qq.com>
2015-09-29 13:21:53 +0800

    bis

--- isell/bis/src/test/java/com/isell/ps/wpwl/controller/WpwlOrderControllerTest.java
+++ isell/bis/src/test/java/com/isell/ps/wpwl/controller/WpwlOrderControllerTest.java
@@ -20,12 +20,12 @@ public class WpwlOrderControllerTest {
     public void testGetOrderByOrderNo() {
         Map<String, String> paramMap = new HashMap<String, String>();
         paramMap.put("service", "wpwl_order_getOrderByOrderNo");
-        paramMap.put("jsonObj", "{\"orderNo\":\"880350384879600241\"}");
+        paramMap.put("jsonObj", "{\"orderNo\":\"test\"}");
         paramMap.put("accessCode", "wpwl");
         paramMap.put("authCode", Coder.encryptBASE64(Coder.encryptMD5(paramMap.get("jsonObj") + "82dd8b166b9748598076e3c2393386fc")));
         
-        String result = HttpUtils.httpPost("http://localhost:8080/bis/gateway", paramMap);
-        //String result = HttpUtils.httpPost("http://service.i-coolshop.com/gateway", paramMap);
+        //String result = HttpUtils.httpPost("http://localhost:8080/bis/gateway", paramMap);
+        String result = HttpUtils.httpPost("http://service.i-coolshop.com/gateway", paramMap);
         System.out.println(result);
     }
     
