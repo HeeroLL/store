19340cc7ff5b5bd0d097365a7c37fc5e316f948a
HeeroLL <114046323@qq.com>
2018-02-23 14:33:01 +0800

    调整

--- antd/src/constants.js
+++
@@ -1,6 +0,0 @@
-// 无需登录校验的URL
-export const LOGIN_UNCHECK_URLS = ['/login'];
-// logo图片cdn路径
-export const LOGO_URL = 'https://app-cdn.starcharge.com/starcharge-logo.png';
-// 系统名称
-export const SYS_NAME = 'SaaS管理系统';
