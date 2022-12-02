02177879d1bad1d919cc14498122fe3f98bc3453
HeeroLL <HeeroLL@users.noreply.github.com>
2022-12-02 17:13:59 +0800

    Delete antd directory

--- antd/src/redux/reducer.js
+++
@@ -1,6 +0,0 @@
-// 合并所有的reducer并返回
-import { combineReducers } from 'redux'
-import {auth} from './login/login.redux.js'
-import {menu} from './menu/menu.redux.js'
-
-export default combineReducers({auth,menu})
--- antd/src/utils/styles.js
+++
@@ -1,4 +0,0 @@
-export const formItemLayout = {
-  labelCol: { span: 4, offset: 2 },
-  wrapperCol: { span: 16 },
-};
