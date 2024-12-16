fca1951fe4b2c183fc6e6f1aa76798fa617b1e35
HeeroLL <HeeroLL@users.noreply.github.com>
2024-12-16 16:36:07 +0800

    Delete webpack-react directory

--- webpack-react/src/carbonShare.js
+++
@@ -1,6 +0,0 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-import Carbonbox from './component/Carbonbox';
-import {v_contextPath} from './config';
-
-ReactDOM.render(<Carbonbox domain={v_contextPath} />, document.getElementsByTagName("body")[0]);
